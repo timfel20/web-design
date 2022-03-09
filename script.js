cc8dfbb291a2c28b03f8cd5089fca01665eb156d
timfel20 <timmyfelicia@gmail.com>
2022-03-10 00:28:10 +0100

    HTML completely setup, all sass components created

---
+++ script.js
@@ -0,0 +1,15 @@
+navbar = document.querySelector('.navbar')
+
+window.addEventListener('scroll', function(e){
+    if (window.scrollY > 0 ) {
+        navbar.classList.add("nav-colored");
+        navbar.classList.remove("nav-transparent");
+     } 
+     else {
+        navbar.classList.add("nav-transparent");
+        navbar.classList.remove("nav-colored");
+     }
+})
+   
+
+console.log(navbar)
